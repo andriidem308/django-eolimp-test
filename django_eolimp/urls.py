80bf6bd4b417c5249cbde7de188f0b3ca9a9af1e
ademchenko <a.demchenko@gethatch.com>
2022-02-02 22:11:39 +0200

    - add home view - renamed urls, views to api_urls, api_views - add urls, views

--- django_eolimp/urls.py
+++ django_eolimp/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('eolimp_app.urls')),
-    path('api/', include('eolimp_app.urls'))
+    path('api/', include('eolimp_app.api_urls'))
 ]
--- eolimp_app/urls.py
+++ eolimp_app/urls.py
@@ -4,16 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('teachers/<int:pk>', TeacherRetrieveView.as_view()),
-    path('teachers/update/<int:pk>', TeacherUpdateView.as_view()),
-    path('teachers/all/', TeacherListView.as_view()),
-    path('teachers/new', TeacherCreateView.as_view()),
-
-    path('students/<int:pk>', StudentRetrieveView.as_view()),
-    path('students/update/<int:pk>', StudentUpdateView.as_view()),
-    path('students/all/', StudentListView.as_view()),
-    path('students/new', StudentCreateView.as_view()),
-
+    path('', home, name='home'),
 
 ]
 
